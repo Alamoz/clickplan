84deb979aa50f02b37bc9ae2e391bb64de83e022
Adrien Lamothe <alamo@deepblue.(none)>
2013-04-20 19:43:10 -0700

    Initial commit of new Click Plan CSS and HTML. This first commit contains the CSS and HTML showing how to create a menu bar with arrows for steps and color highlighting for the current step.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clickplan::Application.config.secret_token = 'c9f9ac271f937cf0e5e4cc19b21bcc123f33bd7110b32e4f8ed0d775da30286d84238a8acc03ec1b5774dfca06bd2612b190b103470ff4c7aaa5edf30146e31b'
